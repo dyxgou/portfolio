46e5d4595b49b8dff48f3f8ea5ada4bd48c74822
dyxgou <diego5614yt@gmail.com>
2025-04-27 09:39:42 -0500

    add(canvas): Creating a somehow natural feeling effect

--- src/components/Canvas/canvas.ts
+++ src/components/Canvas/canvas.ts
@@ -1,3 +1,5 @@
+import Grid from "./grid";
+
 type CanvasParams = {
   ctx: CanvasRenderingContext2D;
   width: number;
@@ -13,15 +15,16 @@ class Canvas {
   height: number;
   cellsSize: number;
   sep: number;
-  minCells: number;
+  grid: Grid;
 
   constructor({ ctx, width, height, cellSize, sep, minCells }: CanvasParams) {
     this.ctx = ctx;
     this.width = width;
     this.height = height;
     this.cellsSize = cellSize;
-    this.minCells = minCells;
     this.sep = sep;
+
+    this.grid = new Grid(this.getRows(), this.getCols(), minCells);
   }
 
   setSize(width: number, height: number) {
@@ -41,31 +44,25 @@ class Canvas {
     return Math.ceil(this.height / (this.cellsSize + this.sep));
   }
 
-  getRandomInteger(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  isActiveCell(): boolean {
-    const random = this.getRandomInteger(0, this.getRows());
-
-    return random <= this.minCells;
-  }
-
   drawSquares() {
-    const cellsRows = this.getRows();
-    const cellsCols = this.getCols();
+    this.grid.generateInitialState();
 
-    for (let j = 0; j < cellsCols; j++) {
-      for (let i = 0; i < cellsRows; i++) {
-        if (this.isActiveCell()) {
+    const cols = this.getCols();
+    const rows = this.getRows();
+
+    for (let y = 0; y < cols; y++) {
+      for (let x = 0; x < rows; x++) {
+        const index = this.grid.getIndex(x, y);
+
+        if (this.grid.getValue(index)) {
           this.setColor("#fe9178");
         } else {
           this.setColor("#f8f8f8");
         }
 
         this.ctx.fillRect(
-          (this.cellsSize + this.sep) * i,
-          (this.cellsSize + this.sep) * j,
+          (this.cellsSize + this.sep) * x,
+          (this.cellsSize + this.sep) * y,
           this.cellsSize,
           this.cellsSize,
         );
