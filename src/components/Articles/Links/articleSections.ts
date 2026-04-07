ea840b1e2dc8962e0f43e90929cf7c8dca438b25
dyxgou <alejandrofullstack@proton.me>
2026-04-07 14:02:39 -0500

    feat(redis): Add Requirements chapter to the Redis Article

---
+++ src/components/Articles/Links/articleSections.ts
@@ -0,0 +1,37 @@
+export type Sections = "introduction" | "requirements" | "resp";
+
+export type ArticleSection = {
+  chapter: number;
+  title: string;
+  href: string;
+  subtitle: string;
+};
+
+const getRedisHREF = (href: Sections) => {
+  const REDIS_HREF = "/projects/redis";
+
+  return `${REDIS_HREF}/${href}`;
+};
+
+const articleSections: Record<Sections, ArticleSection> = {
+  introduction: {
+    chapter: 1,
+    title: "Introduction",
+    href: "/projects/redis/",
+    subtitle: "Brief look onto the Redis Clone",
+  },
+  requirements: {
+    chapter: 2,
+    title: "Requirements",
+    href: getRedisHREF("requirements"),
+    subtitle: "Defining the Basic architecture of Redis",
+  },
+  resp: {
+    chapter: 3,
+    title: "The Redis Serialization Protocol",
+    href: getRedisHREF("resp"),
+    subtitle: "Defining the Basic architecture of Redis",
+  },
+};
+
+export default articleSections;
